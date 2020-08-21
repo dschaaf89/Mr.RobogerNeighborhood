cb92e3e5df9f67d695433ea6a15c6da11c22db88
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-08-21 08:39:02 -0700

    it will pass the second test to replace number 2 and number 1 with strB and Str C.

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,21 @@ function replace(x){
   let strB = "Boop!";
   let strC= "Beep!";
   let newArray=[];
-  
+ 
   for(let i = 0; i <= num; i++){
     newArray.push(i);
-    if(newArray[i] === 3){
+    if(newArray[i] === 1){
+      //  If a user inputs a 5, the program should display a list of values: ""Beep!"", , Boop!, "Won't you be my neighbor?", 4, 5 as 3 is replaced with strC//
+      newArray[i]=strC;
+    }
+    //  If a user inputs a 5, the program should display a list of values: "0", 1, Boop!, "Won't you be my neighbor?", 4, 5 as 3 is replaced with strB//
+    else if(newArray[i] === 2){
+      newArray[i]=strB;
+    }
+    //  If a user inputs a 5, the program should display a list of values: "0", 1, 2, "Won't you be my neighbor?", 4, 5 as 3 is replaced with strA//
+    else if(newArray[i] === 3){
       newArray[i]=strA;
     }
-   }
+  }
   console.log(newArray);
 }
